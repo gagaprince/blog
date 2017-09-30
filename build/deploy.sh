684463da77f03fc6b0a648a55c863cc244af4402
wangzidong <wangzidong@meituan.com>
2017-09-30 13:13:58 +0800

    add rebuild sh

---
+++ build/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /etc/profile
+
+echo "begin rebuild"
+
+echo $PATH
+
+env
+
+cd /root/work/blog/
+
+ls
+
+git pull
+
+echo 'kill blog'
+
+mvn clean -Paliyun package
+
+mv target/ROOT.war /usr/share/tomcat7/webapps/
+
+ps -ef|grep tomcat7 |grep -v grep|awk '{print $2}'|xargs kill -9
+
+cd /usr/share/tomcat7/
+
+sh bin/startup.sh
